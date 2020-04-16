4344d7100ea2e17569ddcd8db1b938999286634a
Filippe Braga <filippeffx@hotmail.com>
2020-04-16 00:13:26 -0300

    Signup

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,53 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp({
   credential: admin.credential.cert(require('./keys/admin.json'))
 });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World");
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyCyXZu48T_WftI5Mx6m5hXle74GhparFSA",
+  authDomain: "socialapp-cbc2c.firebaseapp.com",
+  databaseURL: "https://socialapp-cbc2c.firebaseio.com",
+  projectId: "socialapp-cbc2c",
+  storageBucket: "socialapp-cbc2c.appspot.com",
+  messagingSenderId: "225222865630",
+  appId: "1:225222865630:web:aa481efa0347ae0e7b884f",
+  measurementId: "G-JM21J39LB0"
+};
 
-exports.getScreams = functions.https.onRequest((request, response) => {
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+app.get('/screams', (require, response) => {
   admin
-      .firestore()
-      .collection('screams')
-      .get()
-      .then((data) => {
-          let screams = [];
-          data.forEach((doc) => {
-            screams.push(doc.data());
-          });
+  .firestore()
+  .collection('screams')
+  .orderBy('createdAt', 'desc')
+  .get()
+  .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({ 
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
+      });
 
-          return response.json(screams);
-      })
-      .catch((err) => console.error(err));
+      return response.json(screams);
+  })
+  .catch((err) => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((request, response) => {
+app.post('/scream', (request, response) => {
+
   const newScream = {
     body: request.body.body,
     userHandle: request.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
   admin.firestore()
@@ -46,3 +61,29 @@ exports.createScream = functions.https.onRequest((request, response) => {
         console.error(err);
       });
 });
+
+// Signup route
+app.post('/signup', (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle,
+  }
+
+  // TODO validade data
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return response
+        .status(201)
+        .json({ message: `user ${data.user.uid} signed up successfully.` });
+    })
+    .catch(err => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    })
+})
+
+exports.api = functions.region('us-central1').https.onRequest(app);
